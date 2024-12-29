a53a4ff54059af8fb839583b8151d6509a03516f
Lioh <lithosmances@hotmail.com>
2024-12-29 19:45:53 -0300

    import furniture zzz structured dtype

--- li3dm_zzz_furniture.py
+++ li3dm_zzz_furniture.py
@@ -29,59 +29,30 @@ if __name__ == "__main__":
 
     print(f"Operation completed in {int((time.time()-start)*1000)}ms")
 
-import bpy, numpy
+import bpy, numpy as np
 
-f16 = lambda buf: numpy.frombuffer(buf.read(4), numpy.float16)
-f32 = lambda buf: numpy.frombuffer(buf.read(8), numpy.float32)
-f64 = lambda buf: numpy.frombuffer(buf.read(16), numpy.float64)
+def import_collected_furniture(name:str, vb0_hash:str, vb_fmt="4f2,4i1,4u1,2f,2f2"):
+    vb_file = glob(f"*vb0={vb0_hash}*.buf")[0]
+    vb = np.fromfile(vb_file, np.dtype(vb_fmt))
+    ib = np.fromfile(glob(f"{vb_file[:6]}-ib=*.buf")[0], np.dtype("3u2"))
 
-def import_collected_furniture(path:str, name:str="Furniture", tex_fns=(f32,f32,f16)):
-    vb_file, ib_file = glob(f"{path}/{name}-b0pos*.buf")[0], glob(f"{path}/{name}-b2ib*.buf")[0]
-    with open(vb_file, "rb") as pos, open(ib_file, "rb") as ib:
-        positions, normals, colors = [], [], []
-        uvs = [[] for _ in range(len(tex_fns))]
-        for i in range(os.path.getsize(vb_file) // 28):
-            [px, py, pz, _] = numpy.frombuffer(pos.read(8), numpy.float16)
-            positions.append((-px, -pz, py))
-            [nx, ny, nz, _] = numpy.frombuffer(pos.read(4), numpy.int8) / 127.0
-            normals.append((-nx, -nz, ny))
-            # for uv in uvs:
-            #     uv.append(numpy.frombuffer(pos.read(4), numpy.float16))
-            for j, tex_fn in enumerate(tex_fns):
-                uvs[j].append(tex_fn(pos))
+    def inf(vv): return -vv[0], -vv[2], vv[1]
+    mesh = bpy.data.meshes.new(name)
+    obj = bpy.data.objects.new(mesh.name, mesh)
+    bpy.context.scene.collection.objects.link(obj)
+    mesh.from_pydata([inf(p[0]) for p in vb], [], [list(reversed(p)) for p in ib])
+    mesh.polygons.foreach_set("use_smooth", [True] * len(mesh.polygons))
+    mesh.normals_split_custom_set_from_vertices([inf(p[1] / 127.0) for p in vb])
 
-        contents = numpy.frombuffer(ib.read(), numpy.uint16)
-        faces = [list(reversed(contents[i*3:i*3+3])) for i in range(len(contents)//3)]
+    for i in range(3, len(vb[0])):
+        data = [vb[l.vertex_index][i] for l in mesh.loops]
+        mesh.uv_layers.new().data.foreach_set("uv", np.ravel(data))
 
-        mesh = bpy.data.meshes.new(name)
-        obj = bpy.data.objects.new(mesh.name, mesh)
-        mesh.from_pydata(positions, [], faces)
-        bpy.context.scene.collection.objects.link(obj)
-
-        mesh.polygons.foreach_set("use_smooth", [True] * len(mesh.polygons))
-        mesh.calc_normals_split()
-        mesh.use_auto_smooth = True
-        mesh.normals_split_custom_set_from_vertices(normals)
-
-        for i, uv in enumerate(uvs):
-            uv_layer = mesh.uv_layers.new(name=f"TEXCOORD{i}.xy")
-            for l in mesh.loops:
-                uv_layer.data[l.index].uv = uv[l.vertex_index]
-
-        bpy.context.view_layer.objects.active = obj
-        bpy.ops.object.mode_set(mode='EDIT')
-        bpy.ops.mesh.select_all(action='SELECT')
-        bpy.ops.mesh.remove_doubles(threshold=.000001, use_sharp_edge_from_normals=True)
-        bpy.ops.object.mode_set(mode='OBJECT')
-
-        for diffuse in glob(f"{path}/*t1*.jpg"):
-            mat = bpy.data.materials.new(name=f"LynMaterial")
-            obj.data.materials.append(mat)
-            mat.use_nodes = True
-            bsdf = mat.node_tree.nodes["Principled BSDF"]
-            tex_image = mat.node_tree.nodes.new('ShaderNodeTexImage')
-            tex_image.image = bpy.data.images.load(diffuse)
-            mat.node_tree.links.new(bsdf.inputs['Base Color'], tex_image.outputs['Color'])
+    bpy.context.view_layer.objects.active = obj
+    bpy.ops.object.mode_set(mode='EDIT')
+    bpy.ops.mesh.select_all(action='SELECT')
+    bpy.ops.mesh.remove_doubles(threshold=.000001, use_sharp_edge_from_normals=True)
+    bpy.ops.object.mode_set(mode='OBJECT')
 
 def export_furniture(name:str):
     with (open(f"{name}.buf", "wb") as buf,
@@ -114,3 +85,6 @@ def export_furniture(name:str):
             [u0, v0] = obj.uv_layers[0].data[loop.index].uv
             buf.write(numpy.fromiter([u0, v0], numpy.float16))
             buf.write(numpy.fromiter([u, v], numpy.float16))
+
+os.chdir(r"C:\mod\zzmi\FrameAnalysis-2024-12-28-171842")
+import_collected_furniture("Car2", "a4496188", "4f2,4i1,2f,2f2")
